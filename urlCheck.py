333cd11f322f622306175176e93a46209978f159
K1305739 <TStockbridge531@gmail.com>
2017-10-08 14:33:04 +0100

    UPDATE: Added JSON file streaming and code count
    Commit to implement additional requirements and better reflect main requirements (by splitting address data into individual JSON files).

--- urlCheck.py
+++ urlCheck.py
@@ -4,37 +4,40 @@ import json
 import sys
 import os
 
-filename = raw_input("Please type the file name of your list of web addresses (must be in this script's directory: ")
+filename = raw_input("\nPlease type the file name of your list of web addresses (must be in this script's directory):\n")
 input = open(os.path.join(sys.path[0], filename));
 getInput = input.read().splitlines()
-
 list = []
+
 for line in getInput:
 	list.append(line)
 
-print 'Addresses found:'	
+print 'Addresses found:'
 print list
-
 input.close()
-output = 'output.json'
-out_file = open(output,'w')
+output_code = 'status_code_output.json'
+status_file = open(output_code,'w')
+code_arr = []
+enum = 0;
 
 for item in list:
+	output = 'GET_output_'+str(enum)+'.json'
+	out_file = open(output,'w')
 	x = item.split('://')
 	charCheck = urlparse.urlparse(item)
-	
+
 	if x[0]!='http' and x[0]!='https':
 		sys.stderr.write("ERROR: Web address "+item+" timed out after 10 seconds.\n")
 		json_dict = {
-					'Url': item,
-					'Error': 'Invalid URL: Address was not prefixed with \"http\" or \"https\".'
-				}
+						'Url': item,
+						'Error': 'Invalid URL: Address was not prefixed with \"http\" or \"https\".'
+					}
 	elif bool(charCheck.scheme)==False:
 		sys.stderr.write("ERROR: Web address "+item+" contains character invalid for a URL.\n")
 		json_dict = {
-					'Url': item,
-					'Error': 'Invalid URL: Address contains invalid characters.'
-				}
+						'Url': item,
+						'Error': 'Invalid URL: Address contains invalid characters.'
+					}
 	else:
 		try:
 			getUrl = urllib2.urlopen(item, timeout = 10)
@@ -51,9 +54,29 @@ for item in list:
 							'Content_length': getUrl.headers.get('content-length'),
 							'Date': getUrl.headers.get('date')
 						}
+			#ADDITIONAL REQUIREMENT
+			#########
+			if not code_arr:
+				code_arr.append({'Status_code': getUrl.getcode(),'Number_of_responses': 1})
+			else:
+				code_found = False
+				for i in code_arr:
+					if  i['Status_code'] == getUrl.getcode():
+						code_found = True
+						temp = i['Number_of_responses']
+						i['Number_of_responses'] = temp+1
+						break
+
+				if code_found==False:
+					code_arr.append({'Status_code': getUrl.getcode(),'Number_of_responses': 1})
+			##########
 	print json_dict
 	json.dump(json_dict,out_file,indent=4)
+	enum=enum+1
+	print "\nAddress data saved in JSON document "+output+".\n"	
+	out_file.close()
 
-print '\nOutput saved in JSON document '+output+'.'	
-out_file.close()
-
+print code_arr
+json.dump(code_arr,status_file,indent=4)
+print "\nStatus Code count saved in JSON document "+output_code+".\n"	
+status_file.close()
