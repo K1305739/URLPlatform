2c7d51c829febb11b3abc1583efead1be21b103c
K1305739 <TStockbridge531@gmail.com>
2017-10-08 13:39:34 +0100

    Include URL validation content
    Added If/Else checks to determine the validity of items on the address list. This fix should fulfill the main requirements of the test.

--- urlCheck.py
+++ urlCheck.py
@@ -1,4 +1,5 @@
 import urllib2
+import urlparse
 import json
 import sys
 import os
@@ -10,6 +11,8 @@ getInput = input.read().splitlines()
 list = []
 for line in getInput:
 	list.append(line)
+
+print 'Addresses found:'	
 print list
 
 input.close()
@@ -17,22 +20,40 @@ output = 'output.json'
 out_file = open(output,'w')
 
 for item in list:
-	try:
-		getUrl = urllib2.urlopen(item, timeout = 10)
-	except:
+	x = item.split('://')
+	charCheck = urlparse.urlparse(item)
+	
+	if x[0]!='http' and x[0]!='https':
 		sys.stderr.write("ERROR: Web address "+item+" timed out after 10 seconds.\n")
 		json_dict = {
-						'Url': item,
-						'Error': 'invalid url'
-					}
-	else:
+					'Url': item,
+					'Error': 'Invalid URL: Address was not prefixed with \"http\" or \"https\".'
+				}
+	elif bool(charCheck.scheme)==False:
+		sys.stderr.write("ERROR: Web address "+item+" contains character invalid for a URL.\n")
 		json_dict = {
-						'Url': item,
-						'Status_code': getUrl.getcode(),
-						'Content_length': getUrl.headers.get('content-length'),
-						'Date': getUrl.headers.get('date')
-					}
+					'Url': item,
+					'Error': 'Invalid URL: Address contains invalid characters.'
+				}
+	else:
+		try:
+			getUrl = urllib2.urlopen(item, timeout = 10)
+		except:
+			sys.stderr.write("ERROR: Web address "+item+" timed out after 10 seconds.\n")
+			json_dict = {
+							'Url': item,
+							'Error': 'HTTP GET timed out after 10 seconds.'
+						}
+		else:
+			json_dict = {
+							'Url': item,
+							'Status_code': getUrl.getcode(),
+							'Content_length': getUrl.headers.get('content-length'),
+							'Date': getUrl.headers.get('date')
+						}
 	print json_dict
 	json.dump(json_dict,out_file,indent=4)
+
+print '\nOutput saved in JSON document '+output+'.'	
 out_file.close()
 
